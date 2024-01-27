88c84ff8590f28f21beff7e90fa074c1476c6eab
Jeffrey Jose <jeffjosejeff@gmail.com>
2024-01-27 13:57:58 -0800

    Better styles for contact options

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,6 +2,24 @@
   import { AlertCircle, Github, Globe, Mail } from "lucide-svelte";
   import { Rocket } from "radix-icons-svelte";
   import * as Alert from "$lib/components/ui/alert";
+
+  const items = [
+    {
+      href: "mailto:ggrothau@gmail.com",
+      displayName: "ggrothau@gmail.com",
+      icon: Mail
+    },
+    {
+      href: "https://gregable.com",
+      displayName: "Gregable.com",
+      icon: Globe
+    },
+    {
+      href: "https://github.com/gregable/social-security-tools",
+      displayName: "Gregable/social-security-tools",
+      icon: Github
+    }
+  ];
 </script>
 
 <svelte:head>
@@ -28,21 +46,20 @@
       you spot bugs or have suggestions for things that could be clearer, or
       even just want to say thanks, you can contact me at:
     </p>
-    <ul class="leading-7 [&:not(:first-child)]:mt-6 gap-2 flex flex-col">
-      <li class="flex gap-2 items-center">
-        <Mail class="w-5" />
-        <a href="mailto:ggrothau@gmail.com">ggrothau@gmail.com</a>
-      </li>
-      <li class="flex gap-2 items-center">
-        <Globe class="w-5" />
-        <a href="https://gregable.com">Gregable.com</a>
-      </li>
-      <li class="flex gap-2 items-center">
-        <Github class="w-5" />
-        <a href="https://github.com/Gregable/social-security-tools">
-          Gregable/social-security-tools
+    <ul
+      class="leading-7 [&:not(:first-child)]:mt-6 gap-2 flex flex-col text-sm"
+    >
+      {#each items as item}
+        <a
+          class="flex gap-2 items-center group hover:text-foreground text-muted-foreground transition"
+          href={item.href}
+        >
+          <div class="bg-foreground/5 group-hover:bg-foreground/10 p-2 rounded">
+            <svelte:component this={item.icon} class="w-4 h-4" />
+          </div>
+          <p class="">{item.displayName}</p>
         </a>
-      </li>
+      {/each}
     </ul>
   </div>
 </main>
